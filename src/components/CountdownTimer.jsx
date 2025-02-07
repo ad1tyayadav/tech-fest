76f21ead20c89a87f399963c84ee1e07e79390a8
milliondreamsblog <114696765+milliondreamsblog@users.noreply.github.com>
2025-02-07 09:51:03 +0530

    recent update

--- src/components/CountdownTimer.jsx
+++ src/components/CountdownTimer.jsx
@@ -30,7 +30,12 @@ function CountdownTimer() {
   }, []);
 
   return (
-    <section className="relative top-28 countdown-section text-center py-10">
+    <section className="relative top-10 countdown-section text-center py-10">
+      {/* Green Registration Deadline Banner */}
+      <div className="relative bg-green-600 text-white text-lg sm:text-xl font-bold py-1 mb-10 ">
+        Last Date of Registration: 10th February
+      </div>
+
       <motion.h2
         className="text-3xl sm:text-4xl md:text-5xl text-primary glitch-text text-yellow-500 font-bold"
         initial={{ scale: 0.9, opacity: 0 }}
@@ -59,7 +64,7 @@ function CountdownTimer() {
         ))}
       </motion.div>
       <motion.p
-        className="text-xl mt-4 text-gray-300"
+        className="text-xl mt-4 text-gray-300 mb-8"
         initial={{ scale: 0.9, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
         transition={{ duration: 0.8 }}
@@ -72,13 +77,39 @@ function CountdownTimer() {
         animate={{ scale: 1, opacity: 1 }}
         transition={{ duration: 0.8 }}
       >
-        <h2 className="text-xl font-semibold text-cyan-400 mb-4">Sponsored By</h2>
-        <img
-          src="/inovation.jpg"
-          alt="Funding Cell Logo"
-          className="h-28 w-auto mx-auto rounded-lg"
-        />
-      </motion.section>
+        <h2 className="text-2xl sm:text-4xl md:text-5xl text-primary  text-cyan-500 font-bold mb-8 ">Backed by the Best</h2>
+
+        {/* Logos with Individual Titles */}
+        <div className="flex flex-wrap justify-center items-center gap-12 sm:gap-20">
+          <div className="flex flex-col items-center">
+            <h3 className="text-lg sm:text-xl font-semibold text-yellow-300 mb-4">Innovation Partner</h3>
+            <img
+              src="/inovation.jpg"
+              alt="Innovation Partner"
+              className="h-40 sm:h-44 md:h-48 w-auto rounded-lg"
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <h3 className="text-lg sm:text-xl font-semibold text-yellow-300 mb-4">Title Sponsor</h3>
+            <img
+              src="/Nkosh.png"
+              alt="Title Sponsor"
+              className="h-40 sm:h-44 md:h-48 w-auto rounded-lg"
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <h3 className="text-lg sm:text-xl font-semibold text-yellow-300 mb-4">Institutional Partner</h3>
+            <img
+              src="/csjmuw.png"
+              alt="Institutional Partner"
+              className="h-40 sm:h-44 md:h-48 w-auto rounded-lg"
+            />
+          </div>
+        </div>
+</motion.section>
+
+
+
     </section>
   );
 }
